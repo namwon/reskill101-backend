43433c5035de8f15df8e0b34d4e320d386c0adf2
Sutthanit Chaikhunpol <namwon74@hotmail.com>
2019-07-25 19:25:58 +0700

    addGroup

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom'
 const SignedOutLinks = () => {
    return(
      <ul className="navbar-nav ml-auto">
-        <li className="nav-item"><NavLink className="nav-link" to='/signup'>Signup</NavLink></li>
         <li className="nav-item"><NavLink className="nav-link" to='/signin'>Login</NavLink></li>
      </ul>
    )
 }
 export default SignedOutLinks
+//<li className="nav-item"><NavLink className="nav-link" to='/signup'>Signup</NavLink></li>
