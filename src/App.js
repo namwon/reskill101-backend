0bf709b3914a1e6d8fa60fd3b926931cec8b3081
Sutthanit Chaikhunpol <namwon74@hotmail.com>
2019-07-27 22:47:47 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import MainGroups from './components/Projects/mainGroups'
 import AddGroup from './components/Projects/addGroup'
+import editGroup from './components/Projects/editGroup'
 import VideosAdd from './components/Projects/videosAdd'
 import VideosEdit from './components/Projects/videosEdit'
 import SignIn from './components/auth/SignIn'
@@ -19,6 +20,7 @@ class App extends Component {
             <Route exact path='/' component={MainGroups} />
             <Route path='/mainGroup' component={MainGroups} />
             <Route path='/addGroup' component={AddGroup} />
+            <Route path='/editGroup/:id' component={editGroup} />
             <Route path='/addVideo/:id' component={VideosAdd} />
             <Route path='/item/:id' component={VideosList} />
             <Route path='/video/:id/edit/:eid' component={VideosEdit} />
